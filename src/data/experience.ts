bd12bd9002e737a1f6e340f9bb016caee4097b17
guilherme1s <2000guilherme.silva@gmail.com>
2025-09-20 22:49:35 -0300

    Adiciona arquivo dados de experiencias

---
+++ src/data/experience.ts
@@ -0,0 +1,43 @@
+export const professional = [
+  {
+    id: 1,
+    title: "Thompson Management Horizons",
+    subtitle: "Fevereiro 2025 - presente",
+    role: "Desenvolvedor Full-Stack",
+    topics: [
+      "Experiência com Vue.js e bibliotecas como Vuetify;",
+      "Desenvolvimento de dashboards e gráficos interativos e dinâmicos;",
+      "Sólidos conhecimentos em JavaScript, HTML, CSS e PHP;",
+      "Integração de Front-End com Back-End utilizando Axios e consumo de APIs REST;",
+      "Experiência em modelagem de banco de dados MySQL e execução de queries complexas;",
+      "Prática em usabilidade, UX e UI, focando em interfaces intuitivas;",
+      "Participação em arquitetura de software, definição de soluções e liderança de projetos;",
+      "Atuação em comunicação com clientes, levantamento de requisitos e acompanhamento de entregas;",
+      "Familiaridade com Docker, Git, Figma e boas práticas de versionamento.",
+    ],
+  },
+  {
+    id: 2,
+    title: "Edvac Educação e Informática LTDA",
+    subtitle: "Março 2024 - presente",
+    role: "Estágio em desenvolvimento front-end",
+    topics: [
+      "Experiênca com HTML, CSS e JavaScript para desenvolvimento Front-End;",
+      "Noções de desenvolvimento utilizando React;",
+      "Noções de arquitetura de sistemas e prototipagem de interfaces;",
+      "Experiência com UX, UI, usabilidade, acessibilidade e SEO;",
+      "Familiaridade com versionamento de código usando Git e GitHub;",
+      "Experiência com design de interfaces e prototipagem usando Figma;",
+      "Colaboração com equipe para levantamento de requisitos.",
+    ],
+  },
+];
+
+export const academic = [
+  {
+    id: 1,
+    title: "Centro Universitário Eniac",
+    subtitle: "Concluído em 2024",
+    content: "Bacharelado em Engenharia da Computação"
+  },
+];
